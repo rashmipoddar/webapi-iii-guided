785b72245042d1c93d7dfc2f00055d1b739fa022
Rashmi <poddar.rashmi@gmail.com>
2019-10-30 13:32:08 -0400

    Added custom middleware gatelogger

--- server.js
+++ server.js
@@ -9,8 +9,23 @@ function logger(req, res, next) {
   next();
 }
 
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+  if (!password) {
+    res.status(400).json({ message: 'Please provide password' });  
+  }
+  else if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(401).json({ message: 'You cannot pass!!' });
+  }
+}
+
 server.use(express.json());
 server.use(logger);
+server.use(gateKeeper);
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
